1e52478708fd11ee607c4dd65bd30b220fd5b72a
Samchon <ysgenius88@gmail.com>
2014-02-06 19:30:44 +0900

    WTP

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('sails').lift(require('optimist').argv);
 
 var express = require('express');
 var app = express.createServer(express.logger());
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
