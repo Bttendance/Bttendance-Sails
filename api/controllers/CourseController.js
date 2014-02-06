e13ad3aa1970ce160c534b1d3b1ab7c21502ea02
Samchon <ysgenius88@gmail.com>
2014-02-06 19:32:40 +0900

    WTP

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -7,6 +7,13 @@
 
 var MemJS = require('memjs').Client
 
+var express = require('express');
+var app = express.createServer(express.logger());
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 module.exports = {
 
 	feed: function(req, res) {
