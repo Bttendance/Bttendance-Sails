ce1752896c5638331b31ba55c4d32f99acdb32ea
Samchon <ysgenius88@gmail.com>
2014-02-06 19:39:02 +0900

    remove express

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -7,12 +7,12 @@
 
 var MemJS = require('memjs').Client
 
-var express = require('express');
-var app = express.createServer(express.logger());
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// var express = require('express');
+// var app = express.createServer(express.logger());
+// var port = process.env.PORT || 5000;
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
 
 module.exports = {
 
