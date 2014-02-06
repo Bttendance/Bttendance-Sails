b24bc9cb5ef3db3aec607bd88c9c3d8d5751bb40
Samchon <ysgenius88@gmail.com>
2014-02-06 19:28:11 +0900

    WTP

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // Start sails and pass it command line arguments
 require('newrelic');
 require('sails').lift(require('optimist').argv);
+
+var express = require('express');
+var app = express.createServer(express.logger());
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
