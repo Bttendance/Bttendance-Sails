5244d4e5e22c4b9d87e15212ced436696a10e7d9
Samchon <ysgenius88@gmail.com>
2014-02-06 21:28:45 +0900

    WTP

--- api/controllers/CourseController.js
+++ api/controllers/CourseController.js
@@ -42,7 +42,7 @@ module.exports = {
   			memjs.get(cache_flag, function(err, feeds) {//feed cached
 					if(feeds){//if there is cached feed data, return cached data
 						console.log("hit cache");
-						console.log(feeds);
+						console.log(feeds.length);
 						// var postsJSON = JSON.stringify(feeds);
 						return res.send(feeds);
 					}
--- app.js
+++ app.js
@@ -1,10 +1,3 @@
 // Start sails and pass it command line arguments
 require('newrelic');
 require('sails').lift(require('optimist').argv);
-
-var express = require('express');
-var app = express.createServer(express.logger());
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
