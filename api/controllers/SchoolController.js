65a26889c5d8b3c453cd020f3ef885e259cb17ee
TheFinestArtist <contact@thefinestartist.com>
2014-02-13 01:40:27 +0900

    Added school/courses API

--- api/controllers/SchoolController.js
+++ api/controllers/SchoolController.js
@@ -6,5 +6,30 @@
  */
 
 module.exports = {
-  
+
+	courses: function(req, res) {
+		res.contentType('application/json');
+		var school_id = req.param('school_id');
+
+		School.findOne(school_id).done(function(err, school) {
+			if (err || !school)
+		    return res.send(500, { message: "School Find Error" });
+
+  		Course.find({
+  			where: {
+  				or: getConditionFromIDs(school.courses)
+  			}
+  		}).done(function(err, courses) {
+  			if (err || !courses)
+	    		return res.send(JSON.stringify(new Array()));
+
+  			var coursesObject = new Array();
+				for (var index in courses)
+					coursesObject.push(courses[index]);
+				var coursesJSON = JSON.stringify(coursesObject);
+		  	return res.send(coursesJSON);
+
+  		});
+		});
+	}
 };
