30a10c9497987aaf0a771deeb74ae60061d9fff6
TheFinestArtist <contact@thefinestartist.com>
2014-05-05 08:17:43 +0900

    update

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -568,13 +568,13 @@ module.exports = {
 						var checks = new Array();
 						var attd_check_count = 0;
 						for (var j = 0; j < posts.length; j++) {
-							if (posts[j].type == "attendance") {
+							if (posts[j].course == courses[i].id && posts[j].type == "attendance") {
 								checks = checks.concat(posts[j].attendance.checked_students);
 								attd_check_count++;
 							}
 						}
 						var grade = Number( ( (checks.length - attd_check_count) / attd_check_count / courses[i].students.length * 100).toFixed() );
-  					if (grade  < 0) grade = 0;
+  					if (grade < 0 || attd_check_count == 0) grade = 0;
   					if (grade > 100) grade = 100;
 
 						courses[i] = courses[i].toOldObject();
