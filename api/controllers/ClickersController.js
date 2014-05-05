3d76540da7ff406b5040517855c5f6db3be848dd
TheFinestArtist <contact@thefinestartist.com>
2014-05-05 22:29:33 +0900

    clicker

--- api/controllers/ClickersController.js
+++ api/controllers/ClickersController.js
@@ -5,19 +5,56 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var Error = require('../utils/errors');
+
 module.exports = {
 
 	click: function(req, res) {
 		res.contentType('application/json; charset=utf-8');
+		var username = req.param('username');
 		var clicker_id = req.param('clicker_id');
-		var post_id = req.param('post_id');
+		var choice_number = req.param('choice_number');
 
-    Clickers
-    .update({id:clicker_id}, {post:post_id})
-    .exec(function update(err, clickers) {
-      Clickers.publishUpdate(clickers[0].id, clickers[0].toWholeObject());
-		  return res.send(clickers[0].toWholeObject());
-    });
+		Users
+		.findOneByUsername(username)
+		.exec(function callback(err, user) { 
+			if (err || !user)
+		    return res.send(404, Error.log("User doesn't exitst."));
+
+		  Clickers
+		  .findOneById(clicker_id)
+		  .populate('post')
+		  .exec(function callback(err, clicker) {
+		  	if (err || !clicker)
+			    return res.send(404, Error.log("Clicker doesn't exitst."));
+
+		  	if (choice_number > clicker.choice_count)
+			    return res.send(404, Error.log("Clicker choice is out of bound."));
+
+			  if(choice_number == 1)
+			  	clicker.a_students.push(user.id);
+			  if(choice_number == 2)
+			  	clicker.b_students.push(user.id);
+			  if(choice_number == 3)
+			  	clicker.c_students.push(user.id);
+			  if(choice_number == 4)
+			  	clicker.d_students.push(user.id);
+			  if(choice_number == 5)
+			  	clicker.e_students.push(user.id);
+
+			  clicker.save();
+
+			  Clickers.publishUpdate(clicker_id, { 
+			  	a_students:clicker.a_students  
+			  	b_students:clicker.b_students  
+			  	c_students:clicker.c_students  
+			  	d_students:clicker.d_students  
+			  	e_students:clicker.e_students 
+			  });
+			  
+			  return res.send(clicker.toWholeObject());
+		  });
+		});
 	},
 
 	connect: function(req, res) {
