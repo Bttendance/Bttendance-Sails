43bb67fcc9a86320c61d85013f17a41420c5d44e
TheFinestArtist <contact@thefinestartist.com>
2014-05-10 21:09:14 +0900

    Fixing Socket

--- config/sockets.js
+++ config/sockets.js
@@ -18,8 +18,8 @@ module.exports.sockets = {
     //send back socketId to Client
     var socketID = sails.sockets.id(socket);
     sails.sockets.emit(socketID, 'onConnect', {socketID: socketID});
-    console.log('connect');
-    console.log('socket : ' + JSON.stringify(socket) + ' socketID : ' + socketID);
+    console.log('connect : ' + socketID);
+    console.log(socket);
   },
 
   // This custom onDisconnect function will be run each time a socket disconnects
