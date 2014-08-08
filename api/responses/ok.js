6f6f6b72974d32d170501d07510c6d721dff4723
TheFinestArtist <contact@thefinestartist.com>
2014-08-08 17:43:02 +0900

    update responses for new package

--- api/responses/ok.js
+++ api/responses/ok.js
@@ -4,45 +4,76 @@
  * Usage:
  * return res.ok();
  * return res.ok(data);
- * return res.ok(data, 'auth/login');
+ * return res.ok(data, view);
+ * return res.ok(data, redirectTo);
+ * return res.ok(data, true);
  *
  * @param  {Object} data
- * @param  {String|Object} options
+ * @param  {Boolean|String} viewOrRedirect
+ *         [optional]
  *          - pass string to render specified view
+ *          - pass string with leading slash or http:// or https:// to do redirect
  */
 
-module.exports = function sendOK (data, options) {
+module.exports = function sendOK (data, viewOrRedirect) {
 
-  // Get access to `req`, `res`, & `sails`
+	// Get access to `req` & `res`
   var req = this.req;
   var res = this.res;
-  var sails = req._sails;
 
-  sails.log.silly('res.ok() :: Sending 200 ("OK") response');
+  // Serve JSON (with optional JSONP support)
+  function sendJSON (data) {
+    if (!data) {
+      return res.send();
+    }
+    else {
+      if (typeof data !== 'object') { return res.send(data); }
+      if ( req.options.jsonp && !req.isSocket ) {
+        return res.jsonp(data);
+      }
+      else return res.json(data);
+    }
+  }
 
   // Set status code
   res.status(200);
 
-  // If appropriate, serve data as JSON(P)
-  if (req.wantsJSON) {
-    return res.jsonx(data);
+  // Log error to console
+  this.req._sails.log.verbose('Sent 200 ("OK") response');
+  if (data) {
+    this.req._sails.log.verbose(data);
   }
 
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
+	// Serve JSON (with optional JSONP support)
+	if (req.wantsJSON) {
+		return sendJSON(data);
+	}
 
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
+  // Make data more readable for view locals
+  var locals;
+  if (!data || typeof data !== 'object'){
+    locals = {};
+  }
+  else {
+    locals = data;
   }
 
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  else return res.guessView({ data: data }, function couldNotGuessView () {
-    return res.jsonx(data);
+  // Serve HTML view or redirect to specified URL
+  if (typeof viewOrRedirect === 'string') {
+    if (viewOrRedirect.match(/^(\/|http:\/\/|https:\/\/)/)) {
+      return res.redirect(viewOrRedirect);
+    }
+    else return res.view(viewOrRedirect, locals, function viewReady(viewErr, html) {
+      if (viewErr) return sendJSON(data);
+      else return res.send(html);
+    });
+  }
+  else return res.view(locals, function viewReady(viewErr, html) {
+    if (viewErr) return sendJSON(data);
+    else return res.send(html);
   });
 
 };
+
+
+
