4edb2a08023b5276afd96cf34f9dae7d85e248b1
TheFinestArtist <contact@thefinestartist.com>
2014-08-11 14:20:23 +0900

    fix locale bug

--- api/controllers/TutorialController.js
+++ api/controllers/TutorialController.js
@@ -13,7 +13,7 @@ module.exports = {
 		var app_version = req.param('app_version');
 
 		res.contentType('text/html; charset=utf-8');
-		if (locale == 'kr')
+		if (locale == 'ko')
 			return res.view('tutorial/tutorial-clicker.ejs');
 		else
 			return res.view('tutorial/tutorial-clicker-en.ejs');
@@ -25,7 +25,7 @@ module.exports = {
 		var app_version = req.param('app_version');
 
 		res.contentType('text/html; charset=utf-8');
-		if (locale == 'kr')
+		if (locale == 'ko')
 			return res.view('tutorial/tutorial-attendance.ejs');
 		else
 			return res.view('tutorial/tutorial-attendance-en.ejs');
@@ -37,7 +37,7 @@ module.exports = {
 		var app_version = req.param('app_version');
 
 		res.contentType('text/html; charset=utf-8');
-		if (locale == 'kr')
+		if (locale == 'ko')
 			return res.view('tutorial/tutorial-notice.ejs');
 		else
 			return res.view('tutorial/tutorial-notice-en.ejs');
