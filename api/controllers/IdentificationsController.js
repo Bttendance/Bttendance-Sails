dd9db236665c05ca31332ed86bbbbbf7f41c3b08
TheFinestArtist <contact@thefinestartist.com>
2014-09-19 11:34:27 +0900

    identification bug fix

--- api/controllers/IdentificationsController.js
+++ api/controllers/IdentificationsController.js
@@ -18,21 +18,45 @@ module.exports = {
 		if (!identity)
 			return res.send(400, Error.log(req, "Identity Update Error", "Identity is required."));
 
-		Users.findOneByEmail(email)
+		Users
+		.findOneByEmail(email)
 		.populateAll()
 		.exec(function callback(err, user) {
 			if (err || !user)
 				return res.send(500, Error.log(req, "Identity Update Error", "User doesn't exist."));
 
-			for (var i = 0; i < user.identifications.length; i++)
+			var found = false;
+			for (var i = 0; i < user.identifications.length; i++) {
 				if (user.identifications[i].school == Number(school_id)) {
 					user.identifications[i].identity = identity;
 			  	user.identifications[i].save(function callback(err, updated_user) {
 			  		if (err || !updated_user)
-							return res.send(500, Error.alert(req, "Identity Update Error", "Updating full name has been failed."));
+							return res.send(500, Error.alert(req, "Identity Update Error", "Updating identity has been failed."));
 				  	return res.send(user.toWholeObject());
 			  	});
+					found = true;
+					break;
 				}
+			}
+
+			if (!found) {
+				Identifications.create({
+					identity: identity,
+					school: school_id,
+					owner: user.id
+				}).exec(function callback(err, identification) {
+			  		if (err || !identification)
+							return res.send(500, Error.alert(req, "Identity Update Error", "Updating identity has been failed."));
+						Users
+						.findOneById(user.id)
+						.populateAll()
+						.exec(function callback(err, user) {
+				  		if (err || !user)
+								return res.send(500, Error.log(req, "Identity Update Error", "User doesn't exist."));
+					  	return res.send(user.toWholeObject());
+						});
+				});
+			}
 		});
 	}
 
