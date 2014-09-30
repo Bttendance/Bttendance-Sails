b78bd4a789f768a7ea077802a0d3a78d6771fefe
TheFinestArtist <contact@thefinestartist.com>
2014-10-01 08:24:02 +0900

    updating device

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -330,7 +330,8 @@ module.exports = {
 			  	&& (user.email == 'sayoo123@hanmail.net'
 			  		|| user.email == 'gksekdms21@naver.com'
 			  		|| user.email == 'swyeon11@kaist.ac.kr'
-			  		|| user.email == 'pooreun91@naver.com')) {
+			  		|| user.email == 'pooreun91@naver.com'
+			  		|| user.email == 'john9022@naver.com')) {
 
 					Devices
 					.findOneByUuid(device_uuid)
