bd630ad823248b2c4d23accb5f9c2b0c19d076ce
TheFinestArtist <contact@thefinestartist.com>
2014-11-06 20:01:42 +0900

    updating clicker & courses

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -766,7 +766,6 @@ module.exports = {
 				.populate('attendance')
 				.populate('clicker')
 				.populate('notice')
-				.populate('curious')
 	  		.sort('id DESC')
 	  		.exec(function callback(err, posts) {
 	  			if (!posts) {
