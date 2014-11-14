e4a71e78994ad69e054f7c286a73ef3dacc81d91
TheFinestArtist <contact@thefinestartist.com>
2014-11-14 15:38:21 +0900

    update for test

--- api/controllers/AttendanceClusterController.js
+++ api/controllers/AttendanceClusterController.js
@@ -10,27 +10,25 @@ var Moment = require('moment');
 module.exports = {
 
 	create: function(req, res) {
-		// for (var i = 0; i < 1000; i++) {
-			console.log('start : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			AttendanceCluster.create({
-				attendanceID: 1,
-				email: 'contact@thefinestartist.com',
-				uuid: 'asdf1234'
-			}).exec(function callback(err, attendanceCluster) {
-				console.log('end : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		console.log('start : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		for (var i = 0; i < 1000; i++) {
+			AttendanceCluster
+			.findOneById(1)
+			.exec(function callback(err, attendanceCluster) {
+				console.log('get' + i + ' : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
 			});
-		// }
+		}
 	},
 
 	create2: function(req, res) {
-		// for (var i = 0; i < 1000; i++) {
-			console.log('start : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			Attendances.create({
-				type: 'auto',
-			}).exec(function callback(err, attendances) {
-				console.log('end : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		console.log('start : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		for (var i = 0; i < 1000; i++) {
+			Attendances
+			.findOneById(1)
+			.exec(function callback(err, attendances) {
+				console.log('get' + i + ' : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
 			});
-		// }
+		}
 	}
 	
 };
