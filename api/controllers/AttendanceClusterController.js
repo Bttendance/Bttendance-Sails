b7257b98d90203441cbd651dcbee4735aceea8aa
TheFinestArtist <contact@thefinestartist.com>
2014-11-14 18:04:39 +0900

    updating connect

--- api/controllers/AttendanceClusterController.js
+++ api/controllers/AttendanceClusterController.js
@@ -10,41 +10,42 @@ var Moment = require('moment');
 module.exports = {
 
 	create: function(req, res) {
-		for (var i = 0; i < 1000; i++) {
-			AttendanceCluster.create({
-				attendanceID: 1,
-				email: 'contact@thefinestartist.com',
-				uuid: 'asdf1234'
-			}).exec(function callback(err, attendanceCluster) {
-				console.log(Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			});
-		}
+		AttendanceCluster.create({
+			attendanceID: 1,
+			email: 'contact@thefinestartist.com',
+			uuid: 'asdf1234'
+		}).exec(function callback(err, attendanceCluster) {
+			console.log(Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+  		return res.send(JSON.stringify(attendanceCluster));
+		});
 	},
 
 	create2: function(req, res) {
-		for (var i = 0; i < 1000; i++) {
-			console.log(Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			Attendances
-			.destroy({id:i+1})
-			.exec(function callback(err, attendances) {
-				console.log(Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+
+    for (var i =  1; i <= 200; i++) {
+			var request = require('request');
+			request('http://bttendance-dev.herokuapp.com/attendancecluster/create', function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+		        console.log('success : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		     	} else {
+		        console.log('failure : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		     	}
 			});
-		}
+    };
+
 	},
 
 	feed: function(req, res) {
 
     for (var i =  1; i <= 200; i++) {
-      // setTimeout(function() { 
-				var request = require('request');
-				request('http://bttendance-dev.herokuapp.com/api/courses/feed?email=heehwan.park@bttendance.com&password=sha1$7df4eacb$1$6886cbed77b7cb33d7b9d12986303769dd53647a&course_id=1', function (error, response, body) {
-				    if (!error && response.statusCode == 200) {
-			        console.log('success : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			     	} else {
-			        console.log('failure : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			     	}
-				});
-      // }, i * 50);
+			var request = require('request');
+			request('http://bttendance-dev.herokuapp.com/api/courses/feed?email=heehwan.park@bttendance.com&password=sha1$7df4eacb$1$6886cbed77b7cb33d7b9d12986303769dd53647a&course_id=1', function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+		        console.log('success : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		     	} else {
+		        console.log('failure : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		     	}
+			});
     };
 
 	},
@@ -52,16 +53,14 @@ module.exports = {
 	info: function(req, res) {
 
     for (var i =  1; i <= 200; i++) {
-      // setTimeout(function() { 
-				var request = require('request');
-				request('http://bttendance-dev.herokuapp.com/api/courses/info?email=heehwan.park@bttendance.com&password=sha1$7df4eacb$1$6886cbed77b7cb33d7b9d12986303769dd53647a&course_id=1', function (error, response, body) {
-				    if (!error && response.statusCode == 200) {
-			        console.log('success : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			     	} else {
-			        console.log('failure : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
-			     	}
-				});
-      // }, i * 50);
+			var request = require('request');
+			request('http://bttendance-dev.herokuapp.com/api/courses/info?email=heehwan.park@bttendance.com&password=sha1$7df4eacb$1$6886cbed77b7cb33d7b9d12986303769dd53647a&course_id=1', function (error, response, body) {
+			    if (!error && response.statusCode == 200) {
+		        console.log('success : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		     	} else {
+		        console.log('failure : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+		     	}
+			});
     };
 
 	}
