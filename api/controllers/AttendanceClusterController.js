80e4598dea3fdabbe1c8bf1c81757e577e8873e9
TheFinestArtist <contact@thefinestartist.com>
2014-11-14 15:34:11 +0900

    update for test

--- api/controllers/AttendanceClusterController.js
+++ api/controllers/AttendanceClusterController.js
@@ -10,15 +10,16 @@ var Moment = require('moment');
 module.exports = {
 
 	create: function(req, res) {
-		for (var i = 0; i < 1000; i++) {
+		// for (var i = 0; i < 1000; i++) {
+			console.log('start : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
 			AttendanceCluster.create({
 				attendanceID: 1,
 				email: 'contact@thefinestartist.com',
 				uuid: 'asdf1234'
 			}).exec(function callback(err, attendanceCluster) {
-				console.log(Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
+				console.log('end : ' + Moment().format('YYYY-MM-DD HH:mm ss SSS Z'));
 			});
-		}
+		// }
 	},
 
 	create2: function(req, res) {
